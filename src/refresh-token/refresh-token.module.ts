c875a2045a29a69192d864613edbb327a5edb47a
cjlaflamme1 <chadlaflamme@gmail.com>
2023-02-20 09:34:34 -0500

    docker setup

--- src/refresh-token/refresh-token.module.ts
+++ src/refresh-token/refresh-token.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RefreshTokenService } from './refresh-token.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RefreshToken } from './entities/refresh-token.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RefreshToken])],
+  imports: [TypeOrmModule.forFeature([RefreshToken]), UserModule],
   exports: [RefreshTokenService],
   providers: [RefreshTokenService],
 })
