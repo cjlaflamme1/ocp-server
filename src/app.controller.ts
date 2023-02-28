082caecad7f6208dde3b5f4248a466bdedebf687
cjlaflamme1 <chadlaflamme@gmail.com>
2023-02-27 19:43:14 -0500

    s3 image setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { S3Service } from './services/s3/s3.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private s3Service: S3Service,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('image/:fileName')
+  @UseGuards(JwtAuthGuard)
+  getImagePath(@Param('fileName') fileName: string, fileDirectory: string) {
+    return this.s3Service.getImageObjectSignedUrl(fileName, fileDirectory);
+  }
+
+  @Post('image')
+  @UseGuards(JwtAuthGuard)
+  uploadImagePath(
+    @Body()
+    imageData: {
+      fileName: string;
+      fileType: string;
+      fileDirectory: string;
+    },
+  ) {
+    return this.s3Service.putImageObjectSignedUrl(
+      imageData.fileName,
+      imageData.fileType,
+      imageData.fileDirectory,
+    );
   }
 }
