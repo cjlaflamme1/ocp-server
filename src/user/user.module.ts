e68c5e02bdfdced96313aa8c6fc9aa0ca5a33339
cjlaflamme1 <chadlaflamme@gmail.com>
2023-10-09 17:15:21 -0400

    account delete request

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,11 +6,12 @@ import { User } from './entities/user.entity';
 import { LosenordModule } from 'src/losenord/losenord.module';
 import { S3Service } from 'src/services/s3/s3.service';
 import { DbQueryService } from 'src/services/db-query/db-query.service';
+import { MailerService } from 'src/services/mailer/mailer.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), LosenordModule],
   exports: [UserService],
   controllers: [UserController],
-  providers: [UserService, S3Service, DbQueryService],
+  providers: [UserService, S3Service, DbQueryService, MailerService],
 })
 export class UserModule {}
